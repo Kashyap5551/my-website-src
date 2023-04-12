8ea3a014451a633e817ec3d92531192c6fa8eee1
Kashyap5551 <kash5551@my.yorku.ca>
2023-04-12 16:49:08 -0400

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-52 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
-            Hello, I am Kashyap. My pronouns are He/Him.
+            Hello, I am Kash. I am an aspiring Full-Stack developer.
             <br className="hidden lg:inline-block" />I recently graduated form
             York University with a B.S. in Computer Science.
           </h1>
